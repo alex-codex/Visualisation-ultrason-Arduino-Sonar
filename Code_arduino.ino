fe5e35af9df80934a80789867f5fbdcf495c5c3f
alex-codex <157966891+alex-codex@users.noreply.github.com>
2025-03-09 15:18:29 +0100

    Code pour le capteur ultrason
    la carte utilisée est l'Arduino UNO, le capteur est un HC-SR04

---
+++ Code_arduino.ino
@@ -0,0 +1,31 @@
+unsigned long ultrasonic_Intervalle = 450; // Intervalle de lecture du capteur ultrason
+unsigned long derniere_mesure_distance = 0;
+#define TRIG_PIN 6
+#define ECHO_PIN 7
+
+void setup() {
+   pinMode(TRIG_PIN, OUTPUT);
+   pinMode(ECHO_PIN, INPUT);
+  
+
+}
+
+void loop() {
+   unsigned long temps_actuel = millis();
+   if (temps_actuel - derniere_mesure_distance >= ultrasonic_Intervalle) {
+    derniere_mesure_distance = currentMillis;
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    long duree = pulseIn(ECHO_PIN, HIGH);
+    float distance = duree * 0.034 / 2;
+
+    Serial.print("Distance: ");
+    Serial.println(distance);
+    Serial.println(" cm");
+    
+
+}
